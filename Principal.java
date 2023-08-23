383f0806ab544e88b5c7c4046bd2a0c6caa0f82e
Luis <tluis4671@gmail.com>
2023-08-22 22:37:08 -0500

    Clase Principal Rama 1

--- Principal.java
+++ Principal.java
@@ -22,4 +22,15 @@ public class Principal{
         else
             System.out.println("ups, perdiste!!!");
     }
+    public class Regalo{
+    	public static String elegir(int vida) {
+    		String gift = "";
+    		switch(vida) {
+    		case 1: gift= "Un pasaje al caribe"; break;
+    		case 2: gift= "Una visita al museo mas cercano a tu casa"; break;
+    		case 3: gift= "Una entrada al cine"; break;
+    		}
+    		return gift;
+    	}
+    }
 }
